6fa12b21fd3c63ee1ebcb74be0df08215fe6b9bb
Prasanna <paithankarprasanna@gmail.com>
2023-08-31 14:56:20 +0530

    driver update

--- setupfile.py
+++ setupfile.py
@@ -1,5 +1,6 @@
 import os
 import shutil 
+os.system("python -m pip install --upgrade pip")
 os.system("pip install -r requirements.txt")
 os.system("pyinstaller --onefile --windowed gst_gui.py")
 shutil.move("dist/gst_gui.exe", "GSTscraper.exe")
